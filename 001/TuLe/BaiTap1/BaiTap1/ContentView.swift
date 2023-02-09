281cd45744a2ab74c407c0bb9dc540741206b719
Tu Le C.[2] VN.Danang <tu.le2@monstar-lab.com>
2023-02-09 11:00:05 +0700

    Implement change background color when tap into button

---
+++ 001/TuLe/BaiTap1/BaiTap1/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  BaiTap1
+//
+//  Created by Tu Le C. [2] VN.Danang on 09/02/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var colors: [Color] = [.red, .blue, .black, .orange]
+    @State var backgroundColor: Color = .white
+
+    var body: some View {
+        VStack {
+            Button {
+                guard let element = colors.randomElement() else {
+                    return
+                }
+                backgroundColor = element
+            } label: {
+                Text("Tap")
+                    .padding()
+                    .foregroundColor(.white)
+            }
+            .padding(.horizontal, 20)
+            .background(Color.blue)
+            .clipShape(Capsule())
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(backgroundColor)
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
