7eab0e910bfd80e6067341696a1114cdc53f9599
Khuyen Dang T.T. VN.Danang <khuyen.dang@monstar-lab.com>
2023-02-09 14:16:48 +0700

    Implement about life cycle app

---
+++ 002/KhuyenDang/SwiftUIApp/SwiftUIApp/LifeCycleApp.swift
@@ -0,0 +1,29 @@
+//
+//  LifeCycleApp.swift
+//  SwiftUIApp
+//
+//  Created by Khuyen Dang T.T. VN.Danang on 09/02/2023.
+//
+
+import SwiftUI
+@main
+struct LifeCycleApp: App {
+    @Environment(\.scenePhase) private var scenePhase
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.onChange(of: scenePhase) { phase in
+            switch phase {
+            case .background:
+                print("App State : Background")
+            case .inactive:
+                print("App State : Inactive")
+            case .active:
+                print("App State : Active")
+            @unknown default:
+                print("App State : Unknown")
+            }
+        }
+    }
+}
---
+++ 002/KhuyenDang/SwiftUIApp/SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by Khuyen Dang T.T. VN.Danang on 09/02/2023.
+//
+
+import SwiftUI
+
+//@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
