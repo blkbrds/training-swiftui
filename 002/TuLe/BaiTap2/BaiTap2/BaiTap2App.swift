b9d3a26506a409cb10f2bdefb944249aa1e95533
Tu Le C.[2] VN.Danang <tu.le2@monstar-lab.com>
2023-02-10 11:10:49 +0700

    Implement Life Cycle Apps

--- 002/TuLe/BaiTap2/BaiTap2/BaiTap2App.swift
+++ 002/TuLe/BaiTap2/BaiTap2/BaiTap2App.swift
@@ -9,9 +9,23 @@ import SwiftUI
 
 @main
 struct BaiTap2App: App {
+
+    @Environment(\.scenePhase) private var scenePhase
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }.onChange(of: scenePhase) { phase in
+            switch phase {
+            case .background:
+                print("App State : Background")
+            case .inactive:
+                print("App State : Inactive")
+            case .active:
+                print("App State : Active")
+            @unknown default:
+                print("App State : Unknown")
+            }
         }
     }
 }
