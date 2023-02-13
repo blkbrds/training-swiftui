cc4dd16b179dab4e0fd4c32d754474f769ca43b2
Tu Le C.[2] VN.Danang <tu.le2@monstar-lab.com>
2023-02-13 10:59:09 +0700

    Init project

---
+++ 002/TuLe/BaiTap2/BaiTap2/BaiTap2App.swift
@@ -0,0 +1,17 @@
+//
+//  BaiTap2App.swift
+//  BaiTap2
+//
+//  Created by Tu Le C. [2] VN.Danang on 13/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct BaiTap2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
