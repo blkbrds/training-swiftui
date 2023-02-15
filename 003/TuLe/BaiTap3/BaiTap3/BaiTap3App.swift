a7a033bafdb2a980176af7878219f61b6ee4044e
Tu Le C.[2] VN.Danang <tu.le2@monstar-lab.com>
2023-02-15 08:48:15 +0700

    Implement init project

---
+++ 003/TuLe/BaiTap3/BaiTap3/BaiTap3App.swift
@@ -0,0 +1,17 @@
+//
+//  BaiTap3App.swift
+//  BaiTap3
+//
+//  Created by Tu Le C. [2] VN.Danang on 15/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct BaiTap3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
