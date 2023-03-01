76a74e9b9a59eaa3860d3670e2d6d2c15c4fa3ee
Khuyen Dang T.T. VN.Danang <khuyen.dang@monstar-lab.com>
2023-03-01 15:31:47 +0700

    Implement slider

---
+++ 004/Gesture/Gesture/GestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestureApp.swift
+//  Gesture
+//
+//  Created by Khuyen Dang T.T. VN.Danang on 24/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct GestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MySliderView()
+        }
+    }
+}
