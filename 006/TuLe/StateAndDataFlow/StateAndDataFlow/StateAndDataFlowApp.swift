5cfa1c33ee5344242a6ecb89fb2cc390721754e5
Tu Le C.[2] VN.Danang <tu.le2@monstar-lab.com>
2023-03-26 20:45:07 +0700

    Init project

---
+++ 006/TuLe/StateAndDataFlow/StateAndDataFlow/StateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowApp.swift
+//  StateAndDataFlow
+//
+//  Created by Tu Le C. [2] VN.Danang on 26/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
