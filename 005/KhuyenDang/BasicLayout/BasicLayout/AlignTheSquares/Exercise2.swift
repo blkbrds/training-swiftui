3da486d3ab2946d332259d0bc3820dc285ec7978
Khuyen Dang T.T. VN.Danang <khuyen.dang@monstar-lab.com>
2023-03-31 10:41:52 +0700

    fix bug tranfer from landscape > portrait

--- 005/KhuyenDang/BasicLayout/BasicLayout/AlignTheSquares/Exercise2.swift
+++ 005/KhuyenDang/BasicLayout/BasicLayout/AlignTheSquares/Exercise2.swift
@@ -13,19 +13,25 @@ struct Exercise2: View {
     var widthOfSquare: CGFloat = 70
     
     var body: some View {
-        let spaceForAll = UIScreen.main.bounds.width - (CGFloat(numberOfSquare) * widthOfSquare)
-        let space: CGFloat = spaceForAll / CGFloat((2 + numberOfSquare - 1))
-        HStack(spacing: space) {
-            ForEach(0 ..< numberOfSquare, id: \.self) { _ in
-                SquareBlueView()
+        GeometryReader { geo in
+            VStack {
+                Spacer()
+                let spaceForAll = geo.size.width - (CGFloat(numberOfSquare) * widthOfSquare)
+                let space: CGFloat = spaceForAll / CGFloat((2 + numberOfSquare - 1))
+                HStack(spacing: space) {
+                    ForEach(0 ..< numberOfSquare, id: \.self) { _ in
+                        SquareBlueView()
+                    }
+                }
+                .padding([.leading, .trailing], space)
+                Spacer()
             }
         }
-        .padding([.leading, .trailing], space)
     }
 }
 
 struct Exercise2_Previews: PreviewProvider {
     static var previews: some View {
-        Exercise2().previewDevice("iPhone 8")
+        Exercise2()
     }
 }
