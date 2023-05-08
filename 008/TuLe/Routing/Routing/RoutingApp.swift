c66e7945cbe803e9546bfc68f85e70581ea90bd9
Tu Le C.[2] VN.Danang <tu.le2@monstar-lab.com>
2023-05-08 13:57:58 +0700

    Init project

---
+++ 008/TuLe/Routing/Routing/RoutingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoutingApp.swift
+//  Routing
+//
+//  Created by Tu Le C. [2] VN.Danang on 08/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct RoutingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
