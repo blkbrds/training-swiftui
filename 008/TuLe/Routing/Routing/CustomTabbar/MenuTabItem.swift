83323e3f4279becdefd15d96f75643e61c85ade6
Tu Le C.[2] VN.Danang <tu.le2@monstar-lab.com>
2023-05-17 10:20:05 +0700

    Implement Custom Tabbar

---
+++ 008/TuLe/Routing/Routing/CustomTabbar/MenuTabItem.swift
@@ -0,0 +1,34 @@
+//
+//  MenuTabItem.swift
+//  Routing
+//
+//  Created by Tu Le C. [2] VN.Danang on 17/05/2023.
+//
+
+import SwiftUI
+
+struct MenuTabItem: View {
+    let width, height: CGFloat
+    let systemIconName, tabName: String
+    @ObservedObject var tabbarRouter: TabBarRouter
+    let assignedPage: Page
+
+
+    var body: some View {
+        VStack {
+            Image(systemName: systemIconName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: width, height: height)
+                .padding(.top, 10)
+            Text(tabName)
+                .font(.footnote)
+            Spacer()
+        }
+        .padding(.horizontal, -3)
+        .foregroundColor(tabbarRouter.currentPage == assignedPage ? Color(red: 35 / 255, green: 157 / 255, blue: 251 / 255) : .gray)
+        .onTapGesture {
+            tabbarRouter.currentPage = assignedPage
+        }
+    }
+}
