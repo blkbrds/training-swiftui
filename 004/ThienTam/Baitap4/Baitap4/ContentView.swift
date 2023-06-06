b96050cf4de4a0290ce911e43a4f2353fb36feed
Tam Nguyen K.T. [7] VN.Danang <tam.nguyen7@monstar-lab.com>
2023-06-06 09:45:35 +0700

    done ex4.1

--- 004/ThienTam/Baitap4/Baitap4/ContentView.swift
+++ 004/ThienTam/Baitap4/Baitap4/ContentView.swift
@@ -6,40 +6,48 @@
 //
 
 
-
-
-
-import SwiftUI
-
 import SwiftUI
 
 struct CustomSliderView: View {
     @State private var sliderValue: CGFloat = 0
 
+    var transformedSliderValue: String {
+        let transformedValue = sliderValue + 20
+        return String(format: "%.2f", transformedValue)
+    }
+
     var body: some View {
-        VStack(spacing: 50) {
-            Text("\(Int(sliderValue + 20.0))")
+        VStack(alignment: .center, spacing: 50) {
+            HStack(alignment: .center) {
+                Text("Slider Value:")
+                TextField("Slider value", text:
+                    .constant(transformedSliderValue))
+                .frame(width: 70)
+                    .multilineTextAlignment(.center)
+                    .padding()
+            }
+                .frame(width: 300, height: 50)
 
             ZStack(alignment: .top) {
                 Rectangle()
                     .frame(width: 20, height: 300)
                     .foregroundColor(Color.red)
-                
+
                 Rectangle()
                     .frame(width: 20, height: sliderValue)
                     .foregroundColor(Color.blue)
-                
+
                 Circle()
                     .frame(width: 40, height: 40)
                     .foregroundColor(Color.yellow)
                     .offset(y: sliderValue)
                     .gesture(
-                        DragGesture()
-                            .onChanged { value in
-                                let offsetY = value.location.y
-                                sliderValue = max(-20, min(offsetY, 280))
-                            }
-                    )
+                    DragGesture()
+                        .onChanged { value in
+                        let offsetY = value.location.y
+                        sliderValue = max(-20, min(offsetY, 280))
+                    }
+                )
             }
         }
     }
