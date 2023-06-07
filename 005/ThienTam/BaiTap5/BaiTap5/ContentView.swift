096dca435e1d9908577b5e34a032d2f229fbbffe
Tam Nguyen K.T. [7] VN.Danang <tam.nguyen7@monstar-lab.com>
2023-06-07 11:03:46 +0700

    config ex5

---
+++ 005/ThienTam/BaiTap5/BaiTap5/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BaiTap5
+//
+//  Created by Tam Nguyen K.T. [7] VN.Danang on 07/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 005/ThienTam/BaiTap5/BaiTap5UITests/BaiTap5UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BaiTap5UITestsLaunchTests.swift
+//  BaiTap5UITests
+//
+//  Created by Tam Nguyen K.T. [7] VN.Danang on 07/06/2023.
+//
+
+import XCTest
+
+final class BaiTap5UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
