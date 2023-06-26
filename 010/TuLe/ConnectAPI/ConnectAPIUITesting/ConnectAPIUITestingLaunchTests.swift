2802afcf0ea3f297a99a0e75672ba40907989440
Tu Le C.[2] VN.Danang <tu.le2@monstar-lab.com>
2023-06-26 15:15:18 +0700

    Implement Networking and Unittest and UITest

---
+++ 010/TuLe/ConnectAPI/ConnectAPIUITesting/ConnectAPIUITestingLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ConnectAPIUITestingLaunchTests.swift
+//  ConnectAPIUITesting
+//
+//  Created by Tu Le C. [2] VN.Danang on 26/06/2023.
+//
+
+import XCTest
+
+final class ConnectAPIUITestingLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
