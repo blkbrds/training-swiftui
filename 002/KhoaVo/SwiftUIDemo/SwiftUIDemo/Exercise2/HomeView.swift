207ec366a12c186d11895bcb6afb43c55c66d148
Khoa Vo T.A <khoa.vo@monstar-lab.com>
2023-06-28 10:27:27 +0700

    Implement new life cycle app deep links and alert

---
+++ 002/KhoaVo/SwiftUIDemo/SwiftUIDemo/Exercise2/HomeView.swift
@@ -0,0 +1,27 @@
+//
+//  HomeView.swift
+//  SwiftUIDemo
+//
+//  Created by Khoa Vo T.A. VN.Danang on 15/06/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+
+    @Binding var value: String
+
+    var body: some View {
+        VStack {
+            Text("Home View")
+                .font(.title)
+                .foregroundColor(.purple)
+                .padding()
+            Text("\(value)")
+                .font(.caption)
+                .foregroundColor(.purple)
+                .padding()
+        }
+        .padding()
+    }
+}
