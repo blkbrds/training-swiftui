6c3d0d381dfbd99a9de9b53494814a87458fedbe
Khuyen Dang T.T. VN.Danang <khuyen.dang@monstar-lab.com>
2023-06-29 17:25:06 +0700

    change contentView

--- 010/KhuyenDang/ConnectAPI/ConnectAPI/ContentView.swift
+++ 010/KhuyenDang/ConnectAPI/ConnectAPI/ContentView.swift
@@ -8,35 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-  @StateObject var viewModel = ViewModel()
-    
-  var body: some View {
-    VStack {
-      Image(systemName: "globe")
-        .imageScale(.large)
-        .foregroundColor(.accentColor)
-      Text(viewModel.title)
-        .font(.title)
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
     }
-    .padding()
-  }
-}
-
-extension ContentView {
-  final class ViewModel: ObservableObject {
-    @Published var style: Style = .hello
-        
-    var title: String {
-      switch style {
-      case .hello: return "Hello World!"
-      case .welcome: return "Welcome World!"
-      }
-    }
-  }
-    
-  enum Style {
-    case hello, welcome
-  }
 }
 
 struct ContentView_Previews: PreviewProvider {
