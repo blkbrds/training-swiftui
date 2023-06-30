0d501f2ad62f82291e935165fc5092946ce7b839
Thanh Nguyen X <thanh.nguyen4@monstar-lab.com>
2023-06-30 09:28:56 +0700

    init exercise 2

---
+++ 002/ThanhNguyen/Exercise2/Exercise2/Exercise2App.swift
@@ -0,0 +1,17 @@
+//
+//  Exercise2App.swift
+//  Exercise2
+//
+//  Created by Thanh Nguyen X. [4] VN.Danang on 29/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Exercise2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 002/ThanhNguyen/Exercise2/Exercise2/LifeCycleOfSwiftUIApp.swift
@@ -0,0 +1,8 @@
+//
+//  LifeCycleOfSwiftUIApp.swift
+//  Exercise2
+//
+//  Created by Thanh Nguyen X. [4] VN.Danang on 30/06/2023.
+//
+
+import Foundation
