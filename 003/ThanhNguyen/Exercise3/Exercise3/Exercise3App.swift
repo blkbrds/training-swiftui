fc420fb840a01b8293bb8c7cc1b309ff332dd6e9
Thanh Nguyen X <thanh.nguyen4@monstar-lab.com>
2023-07-03 16:37:10 +0700

    init exercise 3

---
+++ 003/ThanhNguyen/Exercise3/Exercise3/Exercise3App.swift
@@ -0,0 +1,17 @@
+//
+//  Exercise3App.swift
+//  Exercise3
+//
+//  Created by Thanh Nguyen X. [4] VN.Danang on 03/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Exercise3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
