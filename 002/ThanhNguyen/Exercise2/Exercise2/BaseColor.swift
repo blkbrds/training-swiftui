6765a769b28d2812acda1ea9bf892d79aab675d7
Thanh Nguyen X <thanh.nguyen4@monstar-lab.com>
2023-07-03 14:03:25 +0700

    define color

---
+++ 002/ThanhNguyen/Exercise2/Exercise2/BaseColor.swift
@@ -0,0 +1,12 @@
+//
+//  BaseColor.swift
+//  Exercise2
+//
+//  Created by Thanh Nguyen X. [4] VN.Danang on 03/07/2023.
+//
+
+import Foundation
+
+enum BaseColor: String {
+    case titleColor = "titleColor"
+}
