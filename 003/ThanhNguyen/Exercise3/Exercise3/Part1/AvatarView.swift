6f7563bac34c39582c271f8252416aaebf26d97e
Thanh Nguyen X <thanh.nguyen4@monstar-lab.com>
2023-07-04 11:45:08 +0700

    implement part 1 exercise 3

---
+++ 003/ThanhNguyen/Exercise3/Exercise3/Part1/AvatarView.swift
@@ -0,0 +1,50 @@
+//
+//  AvatarView.swift
+//  Exercise3
+//
+//  Created by Thanh Nguyen X. [4] VN.Danang on 04/07/2023.
+//
+
+import SwiftUI
+
+struct AvatarView: View {
+
+    private let name: String = "カイドウ"
+
+    var body: some View {
+        VStack {
+            Image("img_kaido")
+                .resizable()
+                .scaledToFill()
+                .frame(maxWidth: 300, maxHeight: 300)
+                .cornerRadius(.infinity)
+                .onTapGesture {
+                    print("Name: -->", name)
+                }
+            VStack(spacing: 10) {
+                Text("四皇")
+                    .multilineTextAlignment(.center)
+                    .font(.system(
+                        size: 50,
+                        weight: .heavy,
+                        design: .monospaced)
+                    )
+                    .foregroundColor(.red)
+                Text(name)
+                    .multilineTextAlignment(.center)
+                    .font(.system(
+                        size: 35,
+                        weight: .heavy,
+                        design: .monospaced)
+                    )
+            }
+        }
+        .padding()
+    }
+}
+
+struct AvatarView_Previews: PreviewProvider {
+    static var previews: some View {
+        AvatarView()
+    }
+}
