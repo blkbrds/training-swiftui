91c98daad35001a21967685e3d3578c1de44e16e
Thanh Nguyen X <thanh.nguyen4@monstar-lab.com>
2023-07-12 16:46:43 +0700

    refactor welcome screen

--- 003/ThanhNguyen/Exercise3/Exercise3/Part10/WelcomeView.swift
+++ 003/ThanhNguyen/Exercise3/Exercise3/Part10/WelcomeView.swift
@@ -11,26 +11,48 @@ struct WelcomeView: View {
 
     @EnvironmentObject var appRouter: AppRouter
 
+    let numberOfPage: Int = 3
+    var currentPage: Int = 3
+
     var body: some View {
-        VStack {
-            Image("img_welcome")
-                .resizable()
-            ZStack(alignment: .center) {
-                RoundedRectangle(cornerRadius: .infinity)
-                    .stroke(.white, lineWidth: 2)
-                    .frame(height: 50)
-                    .padding()
-                Button {
-                    appRouter.state = .login
-                } label: {
-                    Text("Go to Login screen")
-                        .foregroundColor(.white)
-                        .font(.system(size: 20, weight: .bold))
+        GeometryReader { geo in
+            VStack {
+                Image("img_welcome")
+                    .resizable()
+                    .frame(width: geo.size.width * 2 / 3, height: geo.size.height / 3)
+                VStack {
+                    Text("Make connects with Travello")
+                        .multilineTextAlignment(.leading)
+                        .lineLimit(2)
+                        .font(.system(size: 50, weight: .bold))
+                        .padding(.top, 100)
+                    Text("To your dream trip")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .lineLimit(1)
+                        .font(.system(size: 30, weight: .light))
+                        .padding(.leading)
                 }
+                HStack {
+                    ForEach(0..<numberOfPage, id: \.self) { i in
+                        RoundedRectangle(cornerRadius: .infinity)
+                            .frame(width: 20, height: 10)
+                            .foregroundColor(i + 1 == currentPage ? Color("backgroundPink") : Color("disablePink"))
+                    }
+                    Spacer()
+                    Button(">") {
+                        appRouter.state = .login
+                    }
+                    .frame(width: 60, height: 60)
+                    .background(.black)
+                    .foregroundColor(.white)
+                    .font(.system(size: 25, weight: .black, design: .serif))
+                    .cornerRadius(.infinity)
+                }
+                .padding(.top, 60)
+                .padding([.leading, .trailing])
             }
-            Spacer()
+            .frame(width: geo.size.width, height: geo.size.height)
         }
-        .background(.black)
     }
 }
 
