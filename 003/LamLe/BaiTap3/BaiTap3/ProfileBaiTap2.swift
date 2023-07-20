cb74bfcf44278429e7c2f2a32ced2ceb65331edc
Lam Le T.2 <lam.le2@monstar-lab.com>
2023-07-20 11:43:49 +0700

    Update name and image bai 2

--- 003/LamLe/BaiTap3/BaiTap3/ProfileBaiTap2.swift
+++ 003/LamLe/BaiTap3/BaiTap3/ProfileBaiTap2.swift
@@ -11,13 +11,18 @@ struct ProfileApp: View {
     
     var listName = ["Jack", "Coman", "Messi", "Ronaldo", "Magurie", "Pogba", "Mac", "Mbappe", "Kevin"]
     
+    var listImage = ["Jack", "coman", "messi", "ronaldo", "magurie", "pogba", "mac", "mbappe", "Kevin"]
+    
     var body: some View {
         VStack {
             ForEach(0..<3, id: \.self) { i in
                 HStack {
-                    ProfileView(height: CGFloat(100 + i * 10), width: CGFloat(100 + i * 10), indexView: (i == 0 ? i : i * 3), name: (i == 0 ? listName[i] : listName[i * 3]))
-                    ProfileView(height: CGFloat(100 + i * 10), width: CGFloat(100 + i * 10), indexView: (i == 0 ? i + 1 : i * 3 + 1), name: (i == 0 ? listName[i + 1] : listName[i * 3 + 1]))
-                    ProfileView(height: CGFloat(100 + i * 10), width: CGFloat(100 + i * 10), indexView: (i == 0 ? i + 2 : i * 3 + 2), name: (i == 0 ? listName[i + 2] : listName[i * 3 + 2]))
+                    ProfileView(indexView: (i == 0 ? i : i * 3), name: (i == 0 ? listName[i] : listName[i * 3]), image: (i == 0 ? listImage[i] : listImage[i * 3])
+                    )
+                    ProfileView(indexView: (i == 0 ? i + 1 : i * 3 + 1), name: (i == 0 ? listName[i + 1] : listName[i * 3 + 1]), image: (i == 0 ? listImage[i + 1] : listImage[i * 3 + 1])
+                    )
+                    ProfileView(indexView: (i == 0 ? i + 2 : i * 3 + 2), name: (i == 0 ? listName[i + 2] : listName[i * 3 + 2]), image: (i == 0 ? listImage[i + 2] : listImage[i * 3 + 2])
+                    )
                 }
             }
         }
@@ -32,20 +37,18 @@ struct ProfileApp_Previews: PreviewProvider {
 }
 
 struct ProfileView: View {
-    
-    var height: CGFloat
-    var width: CGFloat
     var indexView: Int
     var name: String
+    var image: String
     
     var body: some View {
         VStack {
-            Image(name)
+            Image(image)
                 .resizable(capInsets: EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
                 .clipShape(Circle())
                 .foregroundColor(.gray)
                 .padding(0)
-                .frame(width: width, height: height)
+                .frame(width: 100, height: 100)
                 .onTapGesture {
                     print("Index View : \(indexView)" )
                 }
