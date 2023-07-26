e2c7c154673bc4beaaa883256f66b14333399390
Thanh Nguyen X <thanh.nguyen4@monstar-lab.com>
2023-07-26 10:01:10 +0700

    init exercise 5

---
+++ 005/ThanhNguyen/Exercise5/Exercise5/Exercise5App.swift
@@ -0,0 +1,17 @@
+//
+//  Exercise5App.swift
+//  Exercise5
+//
+//  Created by Thanh Nguyen X. [4] VN.Danang on 26/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Exercise5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
